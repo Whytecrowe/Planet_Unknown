565b64ad10f89a6a9a749cd5a4712d91df5efb8d
Kirill Caine <dor7an@gmail.com>
2018-03-29 13:54:22 -0700

    after thrust sound

---
+++ Assets/Rocket.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    AudioSource rocketThrust;
+
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        rocketThrust = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))    //can thrust while rotating
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!rocketThrust.isPlaying)
+            {
+                rocketThrust.Play();
+            }
+        }
+        else 
+        {
+            rocketThrust.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
